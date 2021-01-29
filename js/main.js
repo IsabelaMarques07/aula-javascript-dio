813927024be9b2ecf428525131c184072331ac88
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-01-29 18:42:00 -0300

    Aula 1 - Introdução ao Javascript

---
+++ js/main.js
@@ -0,0 +1,13 @@
+var nome = "Isabela Marques"
+var idade = 18
+var idade2 = 21
+var frase = "Japão é o melhor time do mundo"
+//alert(nome+" tem "+idade+" anos.")
+//alert(idade+idade2)
+
+console.log(nome)
+console.log(idade+idade2)
+console.log(frase.replace("Japão", "Brasil"))
+console.log(frase.toLowerCase())
+console.log(frase.toUpperCase())
+//alert(frase.replace("Japão", "Brasil"))
