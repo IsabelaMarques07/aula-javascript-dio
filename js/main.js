8a307fafc62667f0e07417259c7823624a64caa5
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-01-29 19:25:47 -0300

    Aula 4 - Desenvolva páginas web com Javascript

--- js/main.js
+++ js/main.js
@@ -81,3 +81,33 @@ alert(d.getDay())
 alert(d.getHours())
 
 */
+
+/* AULA 4 - DESENVOLVA PÁGINAS WEB COM JAVASCRIPT
+
+function soma(n1, n2){
+    return n1 + n2
+}
+
+alert(soma(5, 10))
+
+function setReplace(frase, nome, novo_nome){
+    return frase.replace(nome, novo_nome)
+}
+
+alert(setReplace("Vai Japão", "Japão", "Brasil"))
+
+function validaIdade(idade){
+    var validar
+    
+    if(idade >= 18){
+        validar = true
+    }else{
+        validar = false
+    }
+    return validar
+}
+
+var idade = prompt ("Qual sua idade?")
+console.log(validaIdade(idade))
+
+*/
