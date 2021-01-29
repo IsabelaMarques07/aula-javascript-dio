fc7e9f9f5de32960901103a66ff6b4403c84a68f
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2021-01-29 18:54:14 -0300

    Aula 2 - Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,13 +1,49 @@
+/* AULA 1 - INTRODUÇÃO AO JAVASCRIPT
+
 var nome = "Isabela Marques"
 var idade = 18
 var idade2 = 21
 var frase = "Japão é o melhor time do mundo"
-//alert(nome+" tem "+idade+" anos.")
-//alert(idade+idade2)
+alert(nome+" tem "+idade+" anos.")
+alert(idade+idade2)
 
 console.log(nome)
 console.log(idade+idade2)
 console.log(frase.replace("Japão", "Brasil"))
 console.log(frase.toLowerCase())
 console.log(frase.toUpperCase())
-//alert(frase.replace("Japão", "Brasil"))
+alert(frase.replace("Japão", "Brasil"))
+*/
+
+/* AULA 2 - ARRAY E DICIONÁRIO
+
+//ARRAY
+var lista = ["maça", "pêra", "laranja"]
+
+console.log(lista)
+console.log(lista[1])
+//alert(lista[1])
+
+lista.pop()
+lista.push("uva")
+console.log(lista)
+
+console.log(lista.length)
+console.log(lista.reverse())
+console.log(lista.toString())
+
+console.log(lista[0])
+console.log(lista.toString()[0])
+
+console.log(lista.join(" - "))
+
+//DICIONARIO
+var fruta = {nome: "Mação", cor:"Vermelha"}
+console.log(fruta.nome)
+console.log(fruta.cor)
+
+//LISTA DE DICIONÁRIOS
+var frutas = [{nome: "Mação", cor:"Vermelha"}, {nome: "Uva", cor:"Roxa"}]
+console.log(frutas)
+console.log(frutas[1].nome)
+*/
